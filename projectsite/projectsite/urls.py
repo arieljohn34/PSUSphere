459e483b13e5a8b097009b554370846815c2293d
arieljohn34 <202280027@psu.palawan.edu.ph>
2026-08-23 23:56:20 +0800

    Update project

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,3 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from studentorg.views import HomePageView
+from studentorg import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+]
+
 """
 URL configuration for projectsite project.
 
@@ -14,9 +25,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
